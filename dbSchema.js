643705001aead3952c71b5b234e8eb2805c12625
ahmedzacky <ahmazaki97@gmail.com>
2020-06-29 22:42:08 +0200

    more routes (signup, login)

---
+++ dbSchema.js
@@ -0,0 +1,14 @@
+// db schema just for visualization
+
+let db = {
+    screams: [
+        {
+            screamID: "Ix0f7vaz7ErBWb9J61Gh",
+            userHandle: "username",
+            body: 'post is post is post',
+            createdAt: '2020-06-29T18:00:58.455Z',
+            likeCount: 5 ,
+            commentCount: 2
+        }
+    ]
+}
